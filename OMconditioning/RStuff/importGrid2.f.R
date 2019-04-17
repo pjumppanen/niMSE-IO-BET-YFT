762bbcca21f1f034fee05d0d6676e0a42fb24fa2
dkolody <dale.kolody@csiro.au>
2019-04-17 11:13:25 +1000

    YFT TCMP 2019 OM and importGrid bug fix
    importGrid2.f bug fix related to importing the best estimates (lowest
    LLH) among converged models

--- OMconditioning/RStuff/importGrid2.f.R
+++ OMconditioning/RStuff/importGrid2.f.R
@@ -8,7 +8,7 @@ importGrid.f <- function(
     convergedNum=0,
     covar=F, exeFile="SS3.24Z",
     stdOnly=F, #only load the OMs with exsiting std file (does not distinguish whether run not done, Hessian failed or Hessian deliberately not calculated)
-    keepEverything=F, #keepEverything added to save re-importing everytime, uses too much memory to do a big list
+    keepEverything=T, #keepEverything added to save re-importing everytime, uses too much memory to do a big list
     doLoad=T, #just check what converged, don't load it
     SPB_Yr="SPB_276",
     ncolSS=213) #this is the BET summary year, not YFT
@@ -16,13 +16,10 @@ importGrid.f <- function(
   loadList <- NULL # if we do not waste time loading the std failures, this is a list of the models actually loaded
   failList <- NULL # models that did not converge
 
-  tmp <- 0
   conStats <- matrix(NA,nrow=0,ncol=6) # statistics for evaluating convergence sensitivity
   colnames(conStats) <- c("modID","iCon", "objFn", "MSY", "SSBYoSSBMSY","maxGrad")
 
   for(i in 1:length(gridList)){
-    rm(tmp)
-    bestOBJfn <- 9.99e+256
     if(convergedNum) {
       convergedDir <- "converged" %&% convergedNum }
     else {
@@ -41,6 +38,8 @@ importGrid.f <- function(
     if(loadFile){
       loadList <- c(loadList, gridList[i])
       if(doLoad & !exists(gridList[i])){   #don't reload if it already exists in memory
+        bestOBJfn <- 9.99e+256
+        best<-tmp<-NULL
         for(iCon in 1:convergedNum){
           tmp <- SS_output2(dir=gridDir  %&% "\\" %&%  gridList[i]  %&% "\\" %&%  "converged" %&% iCon %&% "\\", repfile = "Report.sso", compfile = "CompReport.sso", ncol=ncolSS, covar=covar,forecast=F)
           M <- tmp$endgrowth$M
@@ -54,7 +53,6 @@ importGrid.f <- function(
           maxGrad <- tmp$maximum_gradient_component
 
           conStats <- rbind(conStats,c(gridList[i], iCon, LLH, MSY, SSBYoSSBMSY, maxGrad))
-
           #only retain the full model results from the lowest OBJfn converged result
           if(LLH < bestOBJfn) {
              bestOBJfn <- LLH
@@ -62,18 +60,19 @@ importGrid.f <- function(
              copyStr <- "copy " %&% gridDir  %&%  gridList[i]  %&% "\\" %&%  "converged" %&% iCon %&% "\\*.* " %&% gridDir  %&%  gridList[i]
              shell(copyStr)
 
+             best <- tmp
             if(keepEverything){
-              tmp$M <- M
-              tmp$stdExists <- tmp$stdExists
+              best$M <- M
+              best$stdExists <- stdExists
             } else {
-              tmp <- list(tmp$derived_quants, tmp$recruit, tmp$maximum_gradient_component,tmp$likelihoods_used,tmp$likelihoods_raw_by_fleet,tmp$cpue, tmp$Length_comp_Eff_N_tuning_check, tmp$timeseries[,1:8], tmp$parameters[,1:9],M,
+              best <- list(tmp$derived_quants, tmp$recruit, tmp$maximum_gradient_component,tmp$likelihoods_used,tmp$likelihoods_raw_by_fleet,tmp$cpue, tmp$Length_comp_Eff_N_tuning_check, tmp$timeseries[,1:8], tmp$parameters[,1:9],M,
                  tmp$equil_yield, stdExists )
-              names(tmp) <- c('derived_quants', 'recruit', 'maximum_gradient_component','likelihoods_used',"likelihoods_raw_by_fleet",'cpue', 'Length_comp_Eff_N_tuning_check', 'timeseries', 'parameters','M',
+              names(best) <- c('derived_quants', 'recruit', 'maximum_gradient_component','likelihoods_used',"likelihoods_raw_by_fleet",'cpue', 'Length_comp_Eff_N_tuning_check', 'timeseries', 'parameters','M',
                 'equil_yield', 'stdExists')
             }
           }
         } #iCon
-        assign(gridList[i],tmp, envir = .GlobalEnv)
+        assign(gridList[i],best, envir = .GlobalEnv)
       }
     }
   }
